2d96d2ad3c4fb7023f52b51757094e798dc423f7
Michael Kemna <m.j.kemna@student.tudelft.nl>
2019-08-10 21:26:15 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='powderbooking',
-      version='0.4.0',
+      version='0.4.1',
       description='Application to show the best hotels with the weather',
       classifiers=[
           'Development Status :: 3 - Alpha',
